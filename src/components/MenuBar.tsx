488048e5f8fe7a82c52b6d5878d4e994ac356c48
“kevin-shannon” <kevers429@gmail.com>
2024-09-17 21:08:57 -0500

    pointer hover menubar

---
+++ src/components/MenuBar.tsx
@@ -0,0 +1,19 @@
+import "./MenuBar.css";
+
+function MenuBar() {
+  return (
+    <div className="menu-bar">
+      <div className="menu-bar-item" onClick={() => 2}>
+        <span>Game</span>
+      </div>
+      <div className="menu-bar-item" onClick={() => 2}>
+        <span>Options</span>
+      </div>
+      <div className="menu-bar-item" onClick={() => 2}>
+        <span>About</span>
+      </div>
+    </div>
+  );
+}
+
+export default MenuBar;
